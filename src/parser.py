bd4d3db643efc8b75550b497e20b028c6f652f92
ronitrao222o <Ronitrao2222@gmail.com>
2026-01-28 20:10:26 +0530

    Implement log line and file parsing functions
    Add functions to parse log lines and log files

---
+++ src/parser.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def parse_log_line(line):
+    parts = line.strip().split()
+
+    return {
+        "timestamp": datetime.strptime(parts[0] + " " + parts[1], "%Y-%m-%d %H:%M:%S"),
+        "level": parts[2],
+        "user_id": parts[3].split("=")[1],
+        "endpoint": parts[4].split("=")[1],
+        "latency": int(parts[5].split("=")[1])
+    }
+
+
+def parse_logs(file_path):
+    parsed_logs = []
+    with open(file_path, "r") as file:
+        for line in file:
+            parsed_logs.append(parse_log_line(line))
+    return parsed_logs
